9b1ea0116103603a3a02d7c0627ea43ada7ab490
James <jcoulson95@gmail.com>
2016-11-26 17:29:18 +0000

    Add files via upload
    First Commit to github

---
+++ ECU_DATA/ECU_DATA.ino
@@ -0,0 +1,87 @@
+/* Sketch to get data from CAN bus on Corsa
+ *  Bus speed = 500kbps
+ *  CAN high = Yellow
+ *  CAN low = black
+ */
+
+#include <mcp_can.h>
+#include <SPI.h>
+
+// ECU PID indices
+#define ENGINE_LOAD               0
+#define COOLANT_TEMP              1
+#define FUEL_PRESSURE             2
+#define ENGINE_RPM                3
+#define VEHICLE_KPH               4
+#define TIMING_ADVANCE            5
+#define INTAKE_AIR_TEMP           6
+#define AIR_FLOW_RATE             7
+#define THROTTLE_POS              8
+#define RUNTIME                   9
+#define FUEL_LEVEL                10
+#define ABSOLUTE_PRESSURE         11
+#define OIL_TEMP                  12
+#define FUEL_INJECTION_TIMING     13
+#define FUEL_RATE                 14
+#define TORQUE_PERCENT            15
+#define REFERENCE_TORQUE          16
+
+MCP_CAN CAN0(10);
+
+unsigned char len = 0;
+unsigned char data_in[9];
+long RPM = 0;
+long unsigned int rxId = 0;
+unsigned char led_pins[11] = {15,14,9,8,7,6,5,4,3,2};
+int led_level = 0;
+
+void setup() {
+  Serial.begin(115200);
+  CAN0.begin(CAN_500KBPS, MCP_8MHz); 
+  pinMode(16, INPUT); 
+
+  // setup LED pins
+  for(int n = 0; n<10; n++){
+    pinMode(led_pins[n], OUTPUT);
+    digitalWrite(led_pins[n], LOW);
+  }
+}
+
+void loop() {
+  // request RPM data
+  RPM = requestECUdata(REFERENCE_TORQUE);
+  //printCANMsg(rxId, len, data_in);
+  Serial.print("RPM = ");
+  Serial.println(RPM);
+  // show on leds
+  led_level = (RPM * 10)/4000;
+  setOutput(led_level);
+  delay(100);
+}
+
+void printCANMsg(long unsigned int ID, unsigned char data_count, unsigned char data[9]){
+  // print out data  
+  Serial.print("ID: ");
+  Serial.print(ID, HEX);
+  Serial.print("  Data: ");
+  for(int i = 0; i<data_count; i++){
+    if(data[i] < 0x10){
+      Serial.print("0");
+    }
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print("\t");
+}
+
+void setOutput(int level){
+  if(level > 10){
+    level = 10;
+  }
+  for(int n = 0; n<= level - 1; n++){
+    digitalWrite(led_pins[n], HIGH);
+  }
+  for(int n = level; n<10; n++){
+    digitalWrite(led_pins[n], LOW);
+  }
+}
---
+++ ECU_DATA/ODB_PID_ECU_requests.ino
@@ -0,0 +1,118 @@
+unsigned char ECU_PID[] = {
+  /* Engine Load */                 0x04,
+  /* Engine coolant temp */         0x05,
+  /* Fuel Pressure */               0x0A,
+  /* Engine RPM */                  0x0C,
+  /* Vehicle Speed (kph) */         0x0D,
+  /* Timing Advance */              0x0E,
+  /* Intake Air Temperature */      0x0F,
+  /* Air mass flow rate */          0x10,
+  /* throttle position */           0x11,
+  /* runtime since engine start */  0x1F,
+  /* Fuel Level */                  0x2F,
+  /* Absolute Pressure */           0x33,
+  /* engine oil temperature */      0x5C,
+  /* Fuel injection timing */       0x5D,
+  /* Fuel rate */                   0x5E,
+  /* Torque percentage */           0x62,
+  /* engine reference torque */     0x63,
+};
+
+unsigned char ECU_PID_msglen[] = {
+  /* Engine Load */                 1,
+  /* Engine coolant temp */         1,
+  /* Fuel Pressure */               1,
+  /* Engine RPM */                  2,
+  /* Vehicle Speed (kph) */         1,
+  /* Timing Advance */              1,
+  /* Intake Air Temperature */      1,
+  /* Air mass flow rate */          2,
+  /* throttle position */           1,
+  /* runtime since engine start */  2,
+  /* Fuel Level */                  1,
+  /* Absolute Pressure */           1,
+  /* engine oil temperature */      1,
+  /* Fuel injection timing */       2,
+  /* Fuel rate */                   2,
+  /* Torque percentage */           1,
+  /* engine reference torque */     2,
+};
+
+long requestECUdata(unsigned char pid_index){
+  // create data output buffer
+  unsigned char data_out[9] = {0, 0, 0, 0, 0, 0, 0, 0};
+  // additional data bytes
+  data_out[0] = ECU_PID_msglen[pid_index];
+  // mode
+  data_out[1] = 0x01;
+  // PID
+  data_out[2] = ECU_PID[pid_index];
+  // send data request
+  CAN0.sendMsgBuf(0x7DF, 0, 8, data_out);
+  // wait for response of ID 0x7E8
+  do{
+    while(digitalRead(16));
+    CAN0.readMsgBuf(&len, data_in);
+    rxId = CAN0.getCanId();
+  }while(rxId != 0x7E8);
+  // switch case for different data
+  long output = 0;
+  switch(pid_index){
+    case ENGINE_LOAD:
+      output = (100 * data_in[3]) >> 8;
+      break;
+    case COOLANT_TEMP:
+      output = data_in[3] - 40;
+      break;
+    case FUEL_PRESSURE:
+      output = 3*data_in[3];
+      break;
+    case ENGINE_RPM:
+      output = (data_in[3] << 8) + data_in[4];
+      output = output >> 2;
+      break;
+    case VEHICLE_KPH:
+      output = data_in[3];
+      break;
+    case TIMING_ADVANCE:
+      output = (data_in[3] >> 1) - 64;
+      break;
+    case INTAKE_AIR_TEMP:
+      output = data_in[3] - 40;
+      break;
+    case AIR_FLOW_RATE:
+      output = (data_in[3] << 8) + data_in[4];
+      output = output >> 2;
+      break;
+    case THROTTLE_POS:
+      output = (100 * data_in[3]) >> 8;
+      break;
+    case RUNTIME:
+      output = (data_in[3] << 8) + data_in[4];
+      break;
+    case FUEL_LEVEL:
+      output = (100 * data_in[3]) >> 8;
+      break;
+    case ABSOLUTE_PRESSURE:
+      output = data_in[3];
+      break;
+    case OIL_TEMP:
+      output = data_in[3] - 40;
+      break;
+    case FUEL_INJECTION_TIMING:
+      output = (data_in[3] << 8) + data_in[4];
+      output = (output >> 7) - 210;
+      break;
+    case FUEL_RATE:
+      output = (data_in[3] << 8) + data_in[4];
+      output /= 20;
+      break;
+    case TORQUE_PERCENT:
+      output = data_in[3] - 125;
+      break;
+    case REFERENCE_TORQUE:
+      output = (data_in[3] << 8) + data_in[4];
+      break;
+  }
+  return output;
+}
